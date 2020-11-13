b1b9f7ad9e93fa7aa2f82e854bd3c59cdd18985f
gizmore <gizmore@wechall.net>
2020-11-13 05:11:35 +0100

    6.10-r9372_user_settings_better_and_faster.

--- GDO_Choice.php
+++ GDO_Choice.php
@@ -5,6 +5,7 @@ use GDO\Core\GDO;
 use GDO\DB\GDT_Object;
 use GDO\DB\GDT_AutoInc;
 use GDO\UI\GDT_Title;
+use GDO\DB\GDT_Index;
 
 final class GDO_Choice extends GDO
 {
@@ -12,8 +13,9 @@ final class GDO_Choice extends GDO
 	{
 		return array(
 			GDT_AutoInc::make('choice_id'),
-			GDT_Object::make('choice_poll')->table(GDO_Poll::table())->index()->notNull(),
+			GDT_Object::make('choice_poll')->table(GDO_Poll::table())->notNull(),
 			GDT_Title::make('choice_text')->notNull(),
+		    GDT_Index::make('choice_poll_index')->indexColumns('choice_poll')->hash(),
 		);
 	}
 
