17d7002c1ea0b7e33a27c8d7a83eb02562571667
gizmore <gizmore@wechall.net>
2020-12-20 09:30:56 +0100

    6.10-r9387_backup_and_fixes

--- Method/Overview.php
+++ Method/Overview.php
@@ -2,9 +2,15 @@
 namespace GDO\Poll\Method;
 
 use GDO\Table\MethodQueryList;
+use GDO\Poll\GDO_Poll;
 
+/**
+ * Overview of Polls.
+ * @author gizmore
+ * @since 6.10
+ */
 final class Overview extends MethodQueryList
 {
-	public function gdoTable() {}
-	
+	public function gdoTable() { return GDO_Poll::table(); }
+
 }
